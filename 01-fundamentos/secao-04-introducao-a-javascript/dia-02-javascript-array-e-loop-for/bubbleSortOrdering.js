8449953d9ef336d009a343ceefa800954fc93377
Vicente Vendramin <guesser.vicente@gmail.com>
2022-09-22 19:20:32 -0300

    Add exercise - bubbleSortOrdering

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/bubbleSortOrdering.js
@@ -0,0 +1,38 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if ((index + 1) >= numbers.length) {
+    newNumbers.push(numbers[index] * 2);
+  } else {
+    newNumbers.push(numbers[index] * numbers[index + 1]);
+  }
+}
+
+console.log(newNumbers);
