88d106bb6df11ca4e10548286d74d1e7d171bee1
Vicente Vendramin <guesser.vicente@gmail.com>
2022-09-30 20:34:34 -0300

    Add exercise - part 1

---
+++ 01-fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicio/script.js
@@ -0,0 +1,35 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+function createDaysOfTheMonth() {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDaysList = document.querySelector('#days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    const days = decemberDaysList[index];
+    const decemberDay = document.createElement('li');
+    decemberDay.innerHTML = days;
+    decemberDay.className = 'day';
+
+    if (days === 24 || days === 25 || days === 31) {decemberDay.classList.add('holiday');}
+
+    if (days === 4 || days === 11 || days === 18 || days === 25) {decemberDay.classList.add('friday');}
+
+    monthDaysList.appendChild(decemberDay);
+  };
+}
+
+createDaysOfTheMonth();
