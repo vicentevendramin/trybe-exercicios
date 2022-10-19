d3b3a986d06aa3b484194081938f04de642cc6bb
Vicente Vendramin <guesser.vicente@gmail.com>
2022-10-19 19:24:03 -0300

    Second exercise solved

--- 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitários/dia-03-primeiros-passos-em-jest/exercises/exercises.test.js
+++ 01-fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitários/dia-03-primeiros-passos-em-jest/exercises/exercises.test.js
@@ -1,7 +1,15 @@
 const { sum, myRemove } = require('./exercises.js');
 
+// Exercise 1
 describe('Exercise 1', () => {
   it('Retorno de sum(4, 5) é 9', () => expect(9).toEqual(sum(4, 5)));
   it('Retorno de sum(0, 0) é 0', () => expect(0).toEqual(sum(0, 0)));
-  it('Retorno de sum(4, "5") é 9', () => expect(9).toEqual(sum(4, "5")));
+  // it('Retorno de sum(4, "5") é 9', () => expect(9).toEqual(sum(4, "5")));
+});
+
+// Exercise 2
+describe('Exercise 2', () => {
+  it('Retorno de myRemove([1, 2, 3, 4], 3) é [1, 2, 4]', () => expect([1, 2, 4]).toEqual(myRemove([1, 2, 3, 4], 3)));
+  it('Retorno de myRemove([1, 2, 3, 4], 3) não é [1, 2, 3, 4]', () => expect([1, 2, 3, 4]).not.toEqual(myRemove([1, 2, 3, 4], 3)));
+  it('Retorno de myRemove([1, 2, 3, 4], 5) é [1, 2, 3, 4]', () => expect([1, 2, 3, 4]).toEqual(myRemove([1, 2, 3, 4], 5)));
 });
