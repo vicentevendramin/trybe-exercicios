aa4ab8b760edf1197219541705c0c9eb9f002fca
Vicente Vendramin <guesser.vicente@gmail.com>
2022-11-29 14:50:43 -0300

    Adds three buttons with handleButton function - Fancy Buttons Exercise

--- 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.handleButtonOne = this.handleButtonOne.bind(this);
+    this.handleButtonTwo = this.handleButtonTwo.bind(this);
+    this.handleButtonThree = this.handleButtonThree.bind(this);
+
+    this.state = {
+      clicksBtnOne: 0,
+      clicksBtnTwo: 0,
+      clicksBtnThree: 0,
+    };
+  }
+
+  handleButtonOne() {
+    this.setState((prevState, _props) => ({
+      clicksBtnOne: prevState.clicksBtnOne + 1,
+    }));
+  }
+
+  handleButtonTwo() {
+    this.setState((prevState, _props) => ({
+      clicksBtnTwo: prevState.clicksBtnTwo + 1,
+    }));
+  }
+
+  handleButtonThree() {
+    this.setState((prevState, _props) => ({
+      clicksBtnThree: prevState.clicksBtnThree + 1,
+    }));
+  }
+
+  render() {
+    const { clicksBtnOne, clicksBtnTwo, clicksBtnThree } = this.state;
+    return (
+      <>
+        <button type="button" onClick={ this.handleButtonOne }>{`Clicks: ${ clicksBtnOne }`}</button>
+        <button type="button" onClick={ this.handleButtonTwo }>{`Clicks: ${ clicksBtnTwo }`}</button>
+        <button type="button" onClick={ this.handleButtonThree }>{`Clicks: ${ clicksBtnThree }`}</button>
+      </>
+    );
+  }
 }
 
 export default App;
